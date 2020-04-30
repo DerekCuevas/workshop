84bf82b4cfcdbbf3013ab1a76da2cb71f5c0ff72
Derek Cuevas <derek@secondspectrum.com>
2020-04-29 17:47:33 -0700

    add apollo server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,25 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
+import { ApolloServer, gql } from "apollo-server";
 
-console.log(`Hi ${add(4, 2)}`);
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
