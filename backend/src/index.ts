9db2104a43ffb9f0004d03bc11d7355afaf35caa
Derek Cuevas <derek@secondspectrum.com>
2020-05-01 09:18:02 -0700

    pushing from where we left off - added product schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,32 @@
 import lodash from "lodash";
 import { ApolloServer, gql, makeExecutableSchema } from "apollo-server";
 import { Resolvers } from "./generated/types";
+import * as DAL from "./data-access";
 
 // The GraphQL schema
 const commonTypeDefs = gql`
+  type Product {
+    id: ID!
+    name: String!
+  }
+
   type Query {
     "A simple type for getting started!"
-    hello: String
+    hello: String!
+    products: [Product!]!
   }
 `;
 
 // A map of functions which return data for the schema.
 const commonResolvers: Resolvers = {
+  Product: {
+    id: (root) => root.id,
+    name: (root) => root.name,
+  },
+
   Query: {
     hello: () => "world!",
+    products: () => DAL.getProducts(),
   },
 };
 
